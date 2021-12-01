fd65d7056fe2599a3f4056a4a179f81b5f52dbe1
bannermuere <bannermuere@gmail.com>
2021-11-30 20:02:54 -0600

    commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+});
+
+VanillaTilt.init(document.querySelectorAll(".hover"), {
+    max: 10,
+    speed: 400,
+});
+
+VanillaTilt.init(document.querySelectorAll(".servicio"), {
+    max: 25,
+    speed: 400,
+});
